f498f7234baa94d6d408b9c6c67b49947f8ef1e2
Gervasio Neto <gervasiolopesn@gmail.com>
2025-01-14 10:51:46 -0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,77 @@
+<script>
+
+  import { onMount } from "svelte";
+
+  import { goto } from '$app/navigation';
+  
+	function goToAddToken() {
+	  goto('/AddToken');
+	}
+  
+	function goToEditToken() {
+	  goto('/EditToken');
+	}
+
+  const url = "https://67805ddd85151f714b06955d.mockapi.io/currencies";
+  let currenciesList = []; // Lista de moedas
+
+  // Função para buscar dados da API
+  async function fetchData() {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      currenciesList = data; // Atribui os dados à lista
+    } catch (error) {
+      console.error("Erro ao carregar dados:", error);
+    }
+  }
+
+  // Carrega os dados quando o componente é montado
+  onMount(() => {
+    fetchData();
+  });
+</script>
+
+<main
+  class="m-0 bg-[#13152a] text-white flex justify-center items-center h-screen"
+>
+<div class="text-center w-[550px] p-5 rounded-[10px]">
+    <!-- Logo -->
+    <div class="flex justify-center items-center">
+      <img src="logo.svg" alt="Klever Logo" class="w-[200px] mb-16" />
+    </div>
+
+    <!-- Wish Wallet -->
+    <div class="flex items-center justify-between mb-6">
+      <div class="flex items-center">
+        <img class="w-12 mr-2 mt-1" src="shooting-star.svg" alt="Wish Wallet Icon" />
+        <h1 class="text-4xl font-bold m-0">Wish Wallet</h1>
+      </div>
+      <button on:click={goToAddToken} class="bg-[#aa33b5] text-white px-6 py-2 font-bold text-base rounded-lg">Add Token</button>
+    </div>
+
+    <!-- Table Header -->
+    <div class="flex justify-between mb-3 font-bold text-lg mt-20">
+      <span class="ml-10">Tokens</span>
+      <span>Balance</span>
+    </div>
+
+<!-- List of Currencies -->
+{#each currenciesList as currency (currency.id)}
+  <div class="flex justify-between text-4xl font-bold py-2">
+    <span class="flex items-center">
+      <button 
+        class="bg-transparent border-none cursor-pointer mr-1"
+        on:click={goToEditToken}>
+        <img src="edit-icon.svg" alt="Edit Icon" class="w-5 h-5 mr-4 mt-1" />
+      </button>
+      {currency.name}
+    </span>
+    <span class="">{currency.value.toLocaleString()}</span>
+  </div>
+{/each}
+
+</main>
+
+<style>
+</style>
---
+++ src/routes/AddToken/+page.svelte
@@ -0,0 +1,69 @@
+<script>
+  import { goto } from "$app/navigation";
+
+  function goToHome() {
+    goto("/");
+  }
+</script>
+
+<main
+  class="m-0 bg-[#13152a] text-white flex justify-center items-center h-screen"
+>
+  <div class="text-center w-[550px] p-5 rounded-[10px]">
+    <!-- Logo -->
+    <div class="flex justify-center items-center">
+      <img src="logo.svg" alt="Klever Logo" class="w-[200px] mb-10" />
+    </div>
+
+    <!-- Wish Wallet -->
+    <div class="flex justify-start items-center my-5">
+      <img class="w-12 mr-2 mt-1" src="shooting-star.svg" alt="Wish Wallet Icon" />
+      <h1 class="text-4xl font-bold m-0">Wish Wallet</h1>
+    </div>
+
+    <!-- Add Token e Voltar -->
+    <div class="flex justify-between items-center my-5 ml-10 mb-10 mt-10">
+      <h2 class="text-2xl font-bold">Add Token</h2>
+      <button
+        on:click={goToHome}
+        class="bg-[#646464] text-white border-none px-5 py-2.5 rounded-md cursor-pointer text-sm hover:bg-gray-700 font-bold"
+        >Voltar</button
+      >
+    </div>
+
+    <!-- Formulário -->
+    <form class="flex flex-col gap-1 ml-10">
+      <label class="text-left text-base font-bold text-white " for="token"
+        >Token</label
+      >
+      <input
+        class="p-2.5 text-base border border-gray-700 rounded-md bg-white text-black placeholder-gray-400 mb-10"
+        type="text"
+        id="token"
+        placeholder=""
+        required
+      />
+
+      <label class="text-left text-base font-bold text-white" for="balance"
+        >Balance</label
+      >
+      <input
+        class="p-2.5 text-base border border-gray-700 rounded-md bg-white text-black placeholder-gray-400 mb-10"
+        type="text"
+        id="balance"
+        placeholder=""
+        required
+      />
+
+      <div class="flex justify-between items-center">
+        <button
+          class="bg-[#aa33b5] text-white border-none p-1.5 w-32 rounded-md cursor-pointer text-base font-bold hover:bg-purple-500 ml-auto"
+          >Save</button
+        >
+      </div>
+    </form>
+  </div>
+</main>
+
+<style>
+</style>
